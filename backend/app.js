e708b29df921e5056bc9d23ebb1b4c00200d62a0
Igor Zelenov <igorg.zelenov@gmail.com>
2020-03-09 15:59:59 -0400

    connected

--- backend/app.js
+++ backend/app.js
@@ -1,36 +1,41 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express = require('express');
-const mongoose = require('mongoose');
-const logger = require('morgan');
-const path = require('path');
-const cors = require('cors');
-const session = require('express-session');
-const passport = require('./config/passport');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
+const cors = require("cors");
+const session = require("express-session");
+const passport = require("./config/passport");
 
-
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/ironplate'
-console.log('Connecting DB to ', MONGODB_URI)
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/ironplate";
+console.log("Connecting DB to ", MONGODB_URI);
 
 mongoose
   .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-  .catch((err) => console.error('Error connecting to mongo', err));
+  .then(x =>
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  )
+  .catch(err => console.error("Error connecting to mongo", err));
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const app_name = require("./package.json").name;
+const debug = require("debug")(
+  `${app_name}:${path.basename(__filename).split(".")[0]}`
+);
 
 const app = express();
 
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:3000", "https://distracted-noyce-fee0e2.netlify.com"] //Swap this with the client url 
+    origin: [
+      "http://localhost:3000",
+      "https://naughty-bardeen-1860e3.netlify.com"
+    ] //Swap this with the client url
   })
 );
-
 
 // app.use(cors({
 //   origin: function(origin, callback){
@@ -54,21 +59,21 @@ app.use(passport.session());
 
 //app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static(path.join(__dirname, '../frontend/build')))
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(logger('dev'));
+app.use(logger("dev"));
 
-const index = require('./routes/index');
-const auth = require('./routes/auth');
-app.use('/', index);
-app.use('/', auth);
+const index = require("./routes/index");
+const auth = require("./routes/auth");
+app.use("/", index);
+app.use("/", auth);
 
 // Uncomment this line for production
-let client = path.join(__dirname + '../public/index.html')
-console.log('client',client)
+let client = path.join(__dirname + "../public/index.html");
+console.log("client", client);
 //app.get('*', (req, res) => res.sendFile(client));
 // For any other routes, redirect to the index.html file of React
 // app.get('*', (req, res) => {
--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -1,25 +1,25 @@
-import axios from 'axios';
+import axios from "axios";
 let baseURL;
 
-process.env.NODE_ENV === 'production'
-  //? (baseURL = 'here should be your production endpoint')
-  ? (baseURL = window.location.origin)
-  : (baseURL = 'http://localhost:5000');
+process.env.NODE_ENV === "production"
+  ? //? (baseURL = 'here should be your production endpoint')
+    (baseURL = "https://ironhack-3rd-project.herokuapp.com")
+  : (baseURL = "http://localhost:5000");
 
 const service = axios.create({ withCredentials: true, baseURL });
 
 const actions = {
   isLoggedIn: async () => {
-    return await service.get('/is-logged-in')
+    return await service.get("/is-logged-in");
   },
-  signUp: async (user) => {
-    return await service.post('/signup', user)
+  signUp: async user => {
+    return await service.post("/signup", user);
   },
-  logIn: async (user) => {
-    return await service.post('/login', user)
+  logIn: async user => {
+    return await service.post("/login", user);
   },
   logOut: async () => {
-    return await service.get('/logout')
+    return await service.get("/logout");
   }
 };
 
